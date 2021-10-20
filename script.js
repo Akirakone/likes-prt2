476c5c458730e5f36f500464814315cc6024ec20
Akirakone <ca646@khels.org>
2021-10-20 01:13:21 -0400

    like2

---
+++ script.js
@@ -0,0 +1,8 @@
+function nm(){
+    document.querySelector(".nm").innerText++;
+}
+function ok(){
+    document.querySelector(".ok").innerText++;
+}function cv(){
+    document.querySelector(".cv").innerText++;
+}
